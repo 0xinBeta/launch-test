6cb311cc762816033f52b662f5d38047f1008040
0xinBeta <xterminbeta@protonmail.com>
2024-02-06 23:21:34 +0300

    fix

--- mpl_metadata.ts
+++ mpl_metadata.ts
@@ -18,15 +18,15 @@ const INITIALIZE = true;
 async function main(){
     console.log("let's name some tokens in 2024!");
 const myKeypair = loadWalletKey("/Users/arefgholami/Desktop/NOAAA/codeWith0xin/spl-token/payer2_ny.json");
-const mint = new web3.PublicKey("AzH1YSxnzKhVau6G7dve8NZhWK9JsJ6gVP2AZxPSWEc9");
+const mint = new web3.PublicKey("56zMmCE78LWU6AXc4dgtHJkw9uX8kJHbSJLcBeBVvYd8");
 
     const umi = createUmi("https://mainnet.helius-rpc.com/?api-key=b407c362-c02b-46d7-b828-cd12cd7c89c0");
     const signer = createSignerFromKeypair(umi, fromWeb3JsKeypair(myKeypair))
     umi.use(signerIdentity(signer, true))
 
     const ourMetadata = { // TODO change those values!
-        "name": "DOGM",
-        "symbol": "DM",
+        "name": "Doggo",
+        "symbol": "WOOF",
         uri: "https://raw.githubusercontent.com/0xinbeta/launch-test/main/metadata.json",
     }
     const onChainData = {
