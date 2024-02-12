00aa4f8f0b14fb68748682616f4acfdaba76364c
0xinBeta <xterminbeta@protonmail.com>
2024-02-13 00:30:34 +0300

    fix

--- mpl_metadata.ts
+++ mpl_metadata.ts
@@ -17,16 +17,16 @@ const INITIALIZE = true;
 
 async function main(){
     console.log("let's name some tokens in 2024!");
-const myKeypair = loadWalletKey("/Users/arefgholami/Desktop/NOAAA/codeWith0xin/spl-token/misspiggypu.json");
-const mint = new web3.PublicKey("5pAfQygGqPteGuRwjqmR7WKFK6znSgd2dvzhQ9rQJqzt");
+const myKeypair = loadWalletKey("/Users/arefgholami/Desktop/NOAAA/codeWith0xin/spl-token/sdwau.json");
+const mint = new web3.PublicKey("8CGkA5Stj6iCTs5HGLgoHd3riHfe9shbFnjzRYmAzboJ");
 
     const umi = createUmi("https://mainnet.helius-rpc.com/?api-key=b407c362-c02b-46d7-b828-cd12cd7c89c0");
     const signer = createSignerFromKeypair(umi, fromWeb3JsKeypair(myKeypair))
     umi.use(signerIdentity(signer, true))
 
     const ourMetadata = { // TODO change those values!
-        "name": "Miss Piggy Money",
-        "symbol": "$MPM",
+        "name": "Scooby-Doo, Where Are You!",
+        "symbol": "$SDWAU",
         uri: "https://raw.githubusercontent.com/0xinbeta/launch-test/main/metadata.json",
     }
     const onChainData = {
